48ee272e3587f15bd79b4a68599e7b5f10f29070
KonstantinosDimakis <dimakisblogging@gmail.com>
2017-11-12 13:47:14 +0200

    chore: Scaffold Angular.js app

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name animRadioApp
+ * @description
+ * # animRadioApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('animRadioApp', [
+    'ngAnimate',
+    'ngTouch'
+  ]);
