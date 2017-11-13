68e2af1fdea7777382f551c99493bd9dc8ca6fad
KonstantinosDimakis <dimakisblogging@gmail.com>
2017-11-13 19:40:28 +0200

    feat: Inject and config ui.router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,8 +9,17 @@
  * Main module of the application.
  */
 angular
-  .module('animRadioApp', [
-    'ngAnimate',
-    'ngTouch',
-    'ui.router'
-  ]);
+.module('animRadioApp', [
+  'ngAnimate',
+  'ngTouch',
+  'ui.router'
+])
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'views/main.html'
+  });
+}]);
