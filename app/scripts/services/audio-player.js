5316152ceba17a4f27a652864039bceb64cc4471
KonstantinosDimakis <dimakisblogging@gmail.com>
2017-11-14 18:45:33 +0200

    chore: Introduce audio-player service

---
+++ app/scripts/services/audio-player.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name animRadioApp.audioPlayer
+ * @description
+ * # audioPlayer
+ * Service in the animRadioApp.
+ */
+angular.module('animRadioApp')
+  .service('audioPlayer', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
