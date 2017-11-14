596b0cd72668ce6ae0fb5f981bb1e79d4d42373a
KonstantinosDimakis <dimakisblogging@gmail.com>
2017-11-14 13:35:04 +0200

    chore: Introduce player view & controller

---
+++ app/scripts/controllers/player.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name animRadioApp.controller:PlayerCtrl
+ * @description
+ * # PlayerCtrl
+ * Controller of the animRadioApp
+ */
+angular.module('animRadioApp')
+  .controller('PlayerCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
