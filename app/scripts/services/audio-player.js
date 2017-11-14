c34024f64a53efdc7b46738c895f625ebce5ca0c
KonstantinosDimakis <dimakisblogging@gmail.com>
2017-11-15 01:03:28 +0200

    refactor: Remove unused code

--- app/scripts/services/audio-player.js
+++ app/scripts/services/audio-player.js
@@ -10,5 +10,4 @@
 angular.module('animRadioApp')
   .service('audioPlayer', function () {
     // AngularJS will instantiate a singleton by calling "new" on this function
-    this._player = new Audio();
   });
