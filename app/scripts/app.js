810cd8e3d557d457d863f7176330d3d307ce3a70
KonstantinosDimakis <dimakisblogging@gmail.com>
2017-11-15 23:22:25 +0200

    style: Fix indentation on app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,18 +9,18 @@
  * Main module of the application.
  */
 angular
-.module('animRadioApp', [
-  'ngAnimate',
-  'ngTouch',
-  'ui.router'
-])
-.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/');
+  .module('animRadioApp', [
+    'ngAnimate',
+    'ngTouch',
+    'ui.router'
+  ])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
 
-  $stateProvider
-  .state('player', {
-    url: '/',
-    templateUrl: 'views/player.html',
-    controller: 'PlayerCtrl as player'
-  });
+    $stateProvider
+    .state('player', {
+      url: '/',
+      templateUrl: 'views/player.html',
+      controller: 'PlayerCtrl as player'
+    });
 }]);
