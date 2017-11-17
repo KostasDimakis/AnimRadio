ead249a2ea8a39bd14071ab1e50aa34c161e0d32
KonstantinosDimakis <dimakisblogging@gmail.com>
2017-11-17 22:12:43 +0200

    feat: Implement playing song and pause

--- app/scripts/controllers/player.js
+++ app/scripts/controllers/player.js
@@ -9,5 +9,44 @@
  */
 angular.module('animRadioApp')
   .controller('PlayerCtrl', ['audioPlayer', 'radio', function(player, radio) {
+    this.current = {
+      artist: '',
+      title: '',
+      imgUrl: '',
+      songUrl: ''
+    };
+    this.upcoming = {
+      artist: '',
+      title: '',
+      imgUrl: '',
+      songUrl: ''
+    };
 
+    this.duration = 0;
+    this.time = 0;
+
+    radio.next().then(song => {
+      this.current = song;
+      player.src = song.songUrl;
+      this.play();
+    });
+
+    radio.next().then(song => {
+      this.upcoming = song;
+    });
+
+    this.next = function() {
+      this.current = this.upcoming;
+      radio.next().then(song => {
+        this.upcoming = song;
+      });
+    };
+
+    this.play = function() {
+      player.play();
+    };
+
+    this.pause = function() {
+      player.pause();
+    };
 }]);
