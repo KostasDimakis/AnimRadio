80179fc3fa36c632cf4ff029c8c35c32df4f1fd7
KonstantinosDimakis <dimakisblogging@gmail.com>
2017-12-03 14:09:31 +0200

    feat: Use Firebase for radio service

--- app/scripts/services/radio.js
+++ app/scripts/services/radio.js
@@ -8,8 +8,25 @@
  * Service in the animRadioApp.
  */
 angular.module('animRadioApp')
-  .service('radio', ['$http', function ($http) {
+  .service('radio', ['$firebaseArray', function ($firebaseArray) {
   // AngularJS will instantiate a singleton by calling "new" on this function
+
+  // Initialize Firebase
+  let config = {
+    apiKey: "AIzaSyC-1LbmITfNBMTq_xHOZOpU_5AIY_LFy90",
+    authDomain: "animradio-kostasdimakis.firebaseapp.com",
+    databaseURL: "https://animradio-kostasdimakis.firebaseio.com",
+    projectId: "animradio-kostasdimakis",
+    storageBucket: "animradio-kostasdimakis.appspot.com",
+    messagingSenderId: "913800109016"
+  };
+  firebase.initializeApp(config);
+
+  const ref = firebase.database().ref().child('songs');
+  const storage = firebase.storage();
+
+  this._songs = $firebaseArray(ref);
+
   /**
    * Played songs
    * @type {Array}
@@ -18,45 +35,15 @@ angular.module('animRadioApp')
   this._history = [];
 
   /**
-   *
-   * @type {Promise.<Array>}
-   * @private
-   */
-  this._songs = $http({
-    method: 'GET',
-    url   : '../data.json',
-  }).then(response => {
-      var data = [];
-      response.data.forEach(song => {
-        data.push({
-          artist : song.artist,
-          title  : song.title,
-          // this is needed because they are stored inside my project
-          // in a real scenario it would be a link to a storage
-          imgUrl : window.location.origin + song.imgUrl,
-          songUrl: window.location.origin + song.songUrl,
-        });
-      });
-      return data;
-    }, response => {
-      console.error('Oops, something went wrong: ' + response.status +
-        response.statusText);
-    });
-  /**
    * Return a promise that resolves to a song object
    * @return {Promise.<Object>}
    */
   this.next = function() {
-    return this._songs.then(songs => {
-      // if there are no songs return
-      if (songs.length === 0) {
-        return;
-      }
+    return this._songs.$loaded().then(songs => {
+      let songIndex = getRandomIntInclusive(0, songs.length - 1);
+      this._history.push(songIndex);
 
-      let song = getRandomIntInclusive(0, songs.length - 1);
-      this._history.push(song);
-
-      return songs[song];
+      return convertStorageRefToUrl(songs[songIndex]);
     });
   };
 
@@ -70,6 +57,28 @@ angular.module('animRadioApp')
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+  }
+
+  /**
+   * Convert gs URIs to HTTP URLs
+   * @param {Object} song
+   * @return {Promise.<Object>}
+   */
+  function convertStorageRefToUrl(song) {
+    // resolve the urls
+    const promises = [
+      storage.refFromURL(song.imgLocation).getDownloadURL(),
+      storage.refFromURL(song.songLocation).getDownloadURL()
+    ];
+    // and return a song with the resolved urls
+    return Promise.all(promises).then(data => {
+      return {
+        artist: song.artist,
+        title: song.title,
+        imgUrl: data[0],
+        songUrl: data[1]
+      };
+    });
   }
 
 }]);
